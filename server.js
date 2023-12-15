a74dbdab034c6e52cd0f498a0375dd28201aa7f2
Igor Yasinsky <iyasinskyy@gmail.com>
2023-12-15 19:08:52 +0200

    Made a connection to the backend

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const DB_HOST =
-  'mongodb+srv://Igor:BLymfSk5XItrozHS@cluster0.qczxiic.mongodb.net/contacts_db?retryWrites=true&w=majority';
+const { DB_HOST, PORT = 3000 } = process.env;
 
 // mongoose.set('strictQuery', true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log('Database connection successful');
   })
   .catch((error) => {
