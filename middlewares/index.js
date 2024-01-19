a14b4c42dbf24687dd4700fc63b69db0cab6d21c
Igor Yasinsky <iyasinskyy@gmail.com>
2024-01-19 15:48:29 +0200

    added multer

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require('./validateBody');
 const isValidId = require('./validateId');
 const authentication = require('./authentication');
+const uploud = require('./uploud');
 
 module.exports = {
   validateBody,
   isValidId,
   authentication,
+  uploud,
 };
